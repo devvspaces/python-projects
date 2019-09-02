ca670a8566f1c54e5fb2d7223bbaaf4e07f9c334
Netrob Webby <53845292+n3trob3@users.noreply.github.com>
2019-09-02 03:35:24 +0100

    Time.py reduced coding
    The codes have been reduced to manage space and speed while running

--- time.py
+++ time.py
@@ -3,6 +3,7 @@ def format_duration(seconds):
 		return "0 second"
 	else:
 		min,hr,sec,day,year,m=0,0,seconds,0,0,[]
+		time1=[]
 		while sec > 59:
 			min += sec//60
 			sec = sec % 60
@@ -15,44 +16,19 @@ def format_duration(seconds):
 		while day > 364:
 			year += day//365
 			day = day%365
-		if year != 0:
-			m.append(0)
-		if day != 0:
-			m.append(1)
-		if hr!=0:
-			m.append(2)
-		if min!=0:
-			m.append(3)
-		if sec!=0:
-			m.append(4)
-		if year > 1:
-			year = str(year)+' '+'years'
-		else:
-			year = str(year)+' '+'year'
-		if day > 1:
-			day = str(day)+' '+'days'
-		else:
-			day = str(day)+' '+'day'
-		if hr > 1:
-			hr = str(hr)+' '+'hours'
-		else:
-			hr = str(hr)+' '+'hour'
-		if min > 1:
-			min = str(min)+' '+'minutes'
-		else:
-			min = str(min)+' '+'minute'
-		if sec > 1:
-			sec = str(sec)+' '+'seconds'
-		else:
-			sec = str(sec)+' '+'second'
-		time1 =[year,day,hr,min,sec]
+		for v,i in enumerate([year,day,hr,min,sec]):
+			if i != 0:
+				m.append(v)
+		var=[year,day,hr,min,sec]
+		v1 = ["years","days","hours","minutes","seconds"]
+		v2 = ["year","day","hour","minute","second"]
+		time1+=[str(j)+' '+o if j > 1 else str(j)+' '+l for j,o,l in zip(var,v1,v2)]
 		hu =[]
-		for i in m:
-			hu.append(time1[i])
+		hu+=[time1[i] for i in m]
 		p=", ".join(hu[:len(hu)-1])
 		q = hu[-1]
 		k = [p,q]
 		if p == "":
 			k.remove(p)
 		return " and ".join(k)
-print(format_duration(2384940))
+print(format_duration(365800))
